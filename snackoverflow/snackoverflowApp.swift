05dac67a786e573708c2541d381b29425d339c0d
Erbay MAVZER <mavzerbay@gmail.com>
2023-08-28 11:48:14 +0300

    Initial Commit

---
+++ snackoverflow/snackoverflowApp.swift
@@ -0,0 +1,17 @@
+//
+//  snackoverflowApp.swift
+//  snackoverflow
+//
+//  Created by Erbay MAVZER on 28.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct snackoverflowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
